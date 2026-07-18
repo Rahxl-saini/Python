c6b3ff5c48822626af39a295177b15b34d724c0d
Rahul Saini <rahulsaini2522@gmail.com>
2026-07-18 16:30:19 +0530

    update

--- Pythonfirst.py
+++ Pythonfirst.py
@@ -1,3 +1,10 @@
-print("hari bol")
-print("My name is rahul saini")
-print("I am a Data Engineer")
+# print("hari bol")
+# print("My name is rahul saini")
+# print("I am a Data Engineer")
+
+name = "Rahul Saini"
+age = 22
+price = 25.99
+
+print(name)
+
