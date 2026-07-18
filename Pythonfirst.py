f5131a8ae58509d10749c06df2412db2ce3f5899
Rahul Saini <rahulsaini2522@gmail.com>
2026-07-18 19:39:08 +0530

    update

--- Pythonfirst.py
+++ Pythonfirst.py
@@ -6,7 +6,23 @@ name = "Rahul Saini"
 age = 22
 price = 25.99
 
-print(name)
-print(age)
-print(price)
+# print(name)
+# print(age)
+# print(price)
 
+# print("my ame is ", name)
+# print("my age is", age)
+# print("my price is", price)
+
+# name2 = name
+# print(name2)
+
+# age2 = age
+# print(age2)
+
+# price2 = price
+# print(price2)
+
+print(type(name))
+print(type(age))
+print(type(price))
